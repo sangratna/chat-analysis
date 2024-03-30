fd5118084cae076e446a3dcf6703a784e7637105
Gaikwad Sangratna <139116170+sangratna@users.noreply.github.com>
2024-03-30 09:17:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import preprocessor
 import helper
-from streamlit_option_menu import option_menu
+
 with st.sidebar:
     st.image('https://www.slashgear.com/img/gallery/how-to-enable-encryption-for-your-whatsapp-chat-backup-on-android-or-iphone/l-intro-1692026821.jpg')
-    selected = option_menu('Select Page', ['Introduction page','Analyser'])
+    selected = st.selectbox('Select Page', ['Introduction page', 'Analyser'])
     st.title("Hint")
-    st.info("For analysis open any whatsapp gropu or individual contact-click on 3 dots-select more-export chat-without media-upload below")
+    st.info("For analysis open any WhatsApp group or individual contact, click on 3 dots, select more, export chat (without media), and upload below.")
 
 if selected == 'Introduction page':
     st.title("Welcome to My Introduction Page!")
@@ -18,7 +18,7 @@ if selected == 'Introduction page':
         st.image('https://media.licdn.com/dms/image/D4D03AQEd3wbITMF8HA/profile-displayphoto-shrink_800_800/0/1696757253932?e=2147483647&v=beta&t=iHST64WRZQdpocsNfhMCWhhokMiPUtkwvMn3i3M8TsI', caption="It's Me", use_column_width=True)
     with col2:
         st.write("Hi there! I'm Sangratna Gaikwad, pursuing my M.Tech in IIT Kharagpur majoring in Renewable Energy Technologies.")
-        st.write("Using my this project you can analysy the chats of any Whatsaap group")
+        st.write("Using this project, you can analyze the chats of any WhatsApp group.")
 
     st.subheader("Connect with Me:")
 
@@ -27,7 +27,8 @@ if selected == 'Introduction page':
     st.markdown("🔗 [LinkedIn](https://www.linkedin.com/in/sangratna-gaikwad-395376134)")
     st.markdown("📱 [WhatsApp](tel:+917350521650)")
 
-    st.subheader("Lets Analyse the chats")
+    st.subheader("Let's Analyse the chats")
+
 if selected == 'Analyser':
     st.sidebar.title('WhatsApp Chat Analysis')
     uploaded_file = st.sidebar.file_uploader("Choose a File")
@@ -38,8 +39,8 @@ if selected == 'Analyser':
         data = bytes_data.decode("utf-8")
         df = preprocessor.preprocess(data)
 
-    # Display the preprocessed data
-    # st.dataframe(df)
+        # Display the preprocessed data
+        # st.dataframe(df)
         user_list = df['users'].unique().tolist()
 
         if 'group_notifications' in user_list:
@@ -47,7 +48,7 @@ if selected == 'Analyser':
 
         user_list.sort()
         user_list.insert(0, "Overall")
-        selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
+        selected_user = st.sidebar.selectbox("Show analysis with respect to", user_list)
 
         if st.sidebar.button("Show Analysis"):
             num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user, df)
@@ -78,25 +79,24 @@ if selected == 'Analyser':
             st.pyplot(fig)
 
             st.title('Day wise analysis')
-            col1,col2 = st.columns(2)
+            col1, col2 = st.columns(2)
             with col1:
                 st.header('Most busy days')
-                busy_day = helper.daywise_analysis(selected_user,df)
+                busy_day = helper.daywise_analysis(selected_user, df)
                 fig, ax = plt.subplots()
-                ax.bar(busy_day.index,busy_day.values,color = '#003366')
+                ax.bar(busy_day.index, busy_day.values, color='#003366')
                 plt.xticks(rotation='vertical', fontweight='bold')
                 plt.yticks(fontweight='bold')
                 st.pyplot(fig)
 
-                with col2:
-                    st.header('Monthly Analysis')
-                    busy_month = helper.monthly_analysis(selected_user, df)
-                    fig, ax = plt.subplots()
-                    ax.bar(busy_month.index, busy_month.values,color = '#3cb371')
-                    plt.xticks(rotation='vertical', fontweight='bold')
-                    plt.yticks(fontweight='bold')
-                    st.pyplot(fig)
-
+            with col2:
+                st.header('Monthly Analysis')
+                busy_month = helper.monthly_analysis(selected_user, df)
+                fig, ax = plt.subplots()
+                ax.bar(busy_month.index, busy_month.values, color='#3cb371')
+                plt.xticks(rotation='vertical', fontweight='bold')
+                plt.yticks(fontweight='bold')
+                st.pyplot(fig)
 
             # monthly timeline
             if selected_user == 'Overall':
@@ -122,10 +122,8 @@ if selected == 'Analyser':
             st.pyplot(fig)
 
             most_common_df = helper.most_common_words(selected_user, df)
-        # st.dataframe(most_common_df)
-            fig,ax = plt.subplots()
-            ax.bar(most_common_df[0],most_common_df[1],color = '#00a877')
+            fig, ax = plt.subplots()
+            ax.bar(most_common_df[0], most_common_df[1], color='#00a877')
             ax.set_facecolor('#ffefd5')
             plt.xticks(rotation='vertical', fontsize=11, fontweight='bold', color='navy')
             st.pyplot(fig)
-
